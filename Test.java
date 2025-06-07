804f7dcfadc5aec06fcaa6a6e3e49072580cdfda
BharathMKulkarni <bharathmkulkarni@gmail.com>
2025-06-07 20:38:00 +0530

    new Thread class methods: sleep, setDaemon, join, yield

--- Test.java
+++ Test.java
@@ -9,18 +9,27 @@ public class Test extends Thread {
   @Override
   public void run() {
     int i=0;
-    while(i < 20) {
-      System.out.println(Thread.currentThread().getName() + " is running");
+    while(i < 10) {
+      System.out.println(Thread.currentThread().getName() + " is running " + i);
+
+      // .yield() provides a hint to the scheduler that the current thread is willing to give the CPU time to other threads running.
+      // It is just a hint and not mandatory that it will happen that other threads get the CPU time.
+      Thread.yield();
+      
       i++;
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     System.out.println("Hello world from Main Thread!");
     
     // Creating new Threads using extending Thread class
     Test t1 = new Test("T1");
     Test t2 = new Test("T2");
+    
+    // .setDaemon(true) marks the thread as a Daemon thread. Which means the JVM will not wait for this Thread to finish executing. It might terminate the program before that.
+    // But for regular (User) Threads, which is the default, JVM waits till all these User Threads and the Main Thread are finished executing before terminating the program.
+    t1.setDaemon(true);
     
     // .start() calls the overriden run() method in the subclass of Thread class
     t1.start();
@@ -33,7 +42,7 @@ public class Test extends Thread {
     // MIN_PRIORITY -> 1, NORM_PRIORITY -> 5 (DEFAULT), MAX_PRIORITY -> 10
     // setting a priority is just a hint to the scheduler to prioritize the threads with higher priority.
     // But it is not guranteed that threads with higher priority will always run first. 
-    t1.setPriority(Thread.NORM_PRIORITY);
+    t1.setPriority(Thread.MIN_PRIORITY);
     t2.setPriority(Thread.MAX_PRIORITY);
 
     // .getState() -> returns the current state of the Thread
@@ -42,6 +51,16 @@ public class Test extends Thread {
     // 3. BLOCKED -> Thread is currently waiting for another Thread to finish executing or waiting for some resource
     // 4. TIMED_WAITING -> Thread is waiting for a set amount of time
     // 5. TERMINATED -> Thread has finished executing and has been terminated
+    
+    Thread.sleep(5);
+    System.out.println(t1.getState());
+    System.out.println(t2.getState());
+
+    
+    // .join() stops the current Thread from executing further until the spun Thread completes executing and it joins back to the current Thread.
+    // t1.join();
+    // t2.join();
+
     System.out.println(t1.getState());
     System.out.println(t2.getState());
   }
